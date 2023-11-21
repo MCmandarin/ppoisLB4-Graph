3eff545b6f4a15fe41e0915d232a2c84ad3bb52c
Daniil Pototskiy <daniilpototskiy@mail.ru>
2023-11-21 21:55:12 +0300

    add tests

--- src/lb/DirectedGraphTest.java
+++ src/lb/DirectedGraphTest.java
@@ -1,10 +1,5 @@
-package testGraph;
+package lb;
 
-import lb.AdjacentVerticesIterator;
-import lb.DirectedGraph;
-import lb.Edge;
-import lb.IncidentEdgesIterator;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
